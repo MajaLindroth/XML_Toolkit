cf0ffc043d60445d7926215d6a0e3d2e7b329848
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-07-05 09:56:34 +0100

    Folder structure for unit testing
    + unit testing of some engine methods (convert to point, convert to polyline) This is not done, and we need to review the structure

---
+++ XML_Engine/Test/Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.XML;
+using BHG = BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+
+namespace BH.Adapter.XML
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** PolyLoops                                 ****/
+        /***************************************************/
+
+        public static bool MakeCurveGroup_RandomControlPoints()
+        {
+            //Arrange polyline with random points
+            Random random = new Random();
+            CartesianPoint[] xmlPts = new CartesianPoint[5]; //Test with 5 points
+            List<BHG.Point> ptList = new List<BHG.Point>();
+
+            for (int i = 0; i < xmlPts.Length; i++)
+            {
+                xmlPts[i] = new CartesianPoint();
+                xmlPts[i].Coordinate = new String[3];
+
+                xmlPts[i].Coordinate[0] = random.NextDouble().ToString(); //x
+                xmlPts[i].Coordinate[1] = random.NextDouble().ToString(); //y
+                xmlPts[i].Coordinate[2] = random.NextDouble().ToString(); //z
+
+                BHG.Point bhPoint = Engine.XML.Convert.ToBHoM(xmlPts[i]);
+                ptList.Add(bhPoint);
+            }
+
+            BHG.Polyline refPolyLine = new BHG.Polyline() { ControlPoints = ptList };
+            BHG.PolyCurve refPolyCrv = BH.Engine.Geometry.Create.PolyCurve(new List<BHG.Polyline>() { refPolyLine });
+
+
+            //Act
+            Polyloop xmlPLoop = new Polyloop() { CartesianPoint = xmlPts };
+            BHG.PolyCurve testCurve = Adapter.gbXML.gbXMLDeserializer.MakeCurveGroup(xmlPLoop);
+
+            //Tests.
+            bool test1 = ((testCurve.ControlPoints().Count - 1) == xmlPLoop.CartesianPoint.Length); //Amount of controlpoints: The xml PolyLoop should not contain the first point at the last index whereas the BHoM Polycurve should.
+            bool test2 = true;
+            for (int i = 0; i < refPolyCrv.ControlPoints().Count; i++)
+            {
+                test2 &= refPolyCrv.ControlPoints()[i] == testCurve.ControlPoints()[i];
+                if (!test2) break;
+            }
+
+            return (test1 && test2);
+        }
+        /***************************************************/
+    }
+}
--- XML_Test/Adapter/CRUD/ReadTest.cs
+++ XML_Test/Adapter/CRUD/ReadTest.cs
@@ -8,47 +8,65 @@ using BHG = BH.oM.Geometry;
 using BH.Engine.Geometry;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+//TEST namespace BH.Engine.XML and do Ctrl+b test
 namespace BH.Test.XML
 {
     [TestClass]
-    public partial class Read
+    public partial class Query
     {
         /***************************************************/
         /**** PolyLoops                                 ****/
         /***************************************************/
 
+        //    [TestMethod]
+        //    public void MakeCurveGroup_RandomControlPoints()
+        //    {
+        //        //Arrange polyline with random points
+        //        Random random = new Random();
+        //        CartesianPoint[] xmlPts = new CartesianPoint[5]; //Test with 5 points
+        //        List<BHG.Point> ptList = new List<BHG.Point>();
+
+        //        for (int i = 0; i < xmlPts.Length; i++)
+        //        {
+        //            xmlPts[i] = new CartesianPoint();
+        //            xmlPts[i].Coordinate = new String[3];
+
+        //            xmlPts[i].Coordinate[0] = random.NextDouble().ToString(); //x
+        //            xmlPts[i].Coordinate[1] = random.NextDouble().ToString(); //y
+        //            xmlPts[i].Coordinate[2] = random.NextDouble().ToString(); //z
+
+        //            BHG.Point bhPoint = Engine.XML.Convert.ToBHoM(xmlPts[i]);
+        //            ptList.Add(bhPoint);
+        //        }
+
+        //        BHG.Polyline refPolyLine = new BHG.Polyline() { ControlPoints = ptList };
+        //        BHG.PolyCurve refPolyCrv = BH.Engine.Geometry.Create.PolyCurve( new List<BHG.Polyline>() { refPolyLine });
+
+
+        //        //Act
+        //        Polyloop xmlPLoop = new Polyloop() { CartesianPoint = xmlPts };
+        //        BHG.PolyCurve testCurve = Adapter.gbXML.gbXMLDeserializer.MakeCurveGroup(xmlPLoop);
+
+        //        //Tests.
+        //        bool test1 = ((testCurve.ControlPoints().Count - 1) == xmlPLoop.CartesianPoint.Length); //Amount of controlpoints: The xml PolyLoop should not contain the first point at the last index whereas the BHoM Polycurve should.
+        //        bool test2 = true;
+        //        for (int i = 0; i < refPolyCrv.ControlPoints().Count; i++)
+        //        {
+        //            test2 &= refPolyCrv.ControlPoints()[i] == testCurve.ControlPoints()[i];
+        //            if (!test2) break;
+        //        }
+
+        //        //Assert
+        //        Assert.IsTrue(test1 && test2);
+        //    }
+        //    /***************************************************/
+        //}
+
         [TestMethod]
-        public void MakeCurveGroup_RandomControlPoints()
+        public void MakeCurveGroup_RandomControlPoints2()
         {
-            //Arrange
-            Random random = new Random();
-            CartesianPoint[] xmlPts = new CartesianPoint[5]; //Test with 5 points
-
-            for (int i = 0; i < xmlPts.Length; i++)
-            {
-                xmlPts[i] = new CartesianPoint();
-                xmlPts[i].Coordinate = new String[3];
-
-                xmlPts[i].Coordinate[0] = 1.ToString();
-                xmlPts[i].Coordinate[1] = 2.ToString();
-                xmlPts[i].Coordinate[2] = 3.ToString();
-
-                BHG.Point bhPoint = Engine.XML.Convert.ToBHoM(xmlPts[i]);
-
-            }
-            
-            //Act
-            Polyloop xmlPLoop = new Polyloop() { CartesianPoint = xmlPts };
-            BHG.PolyCurve bhPCurve = Adapter.gbXML.gbXMLDeserializer.MakeCurveGroup(xmlPLoop);
-
-            //Assert - controll amount of Points. Control all of the points
-            BHG.Polyline pline = bhPCurve.CollapseToPolyline(1e-06);
-            Assert.IsTrue(pline.ControlPoints.Count-1 == xmlPLoop.CartesianPoint.Length);
-
-            //TODO: Add more checks to see if the polyline matches the polyloop. And compare the pCurve (and not the pline)
-          
+            Assert.IsTrue(BH.Engine.XML.Test.MakeCurveGroup_RandomControlPoints());
         }
-
         /***************************************************/
     }
 }

ff8ca889c14b9bf9e9110b05f9b58a55b858a02a
Maria Tjader <maria.tjader@burohappold.com>
2018-09-27 11:47:13 +0100

    Continuing work on XMLAdaptertest, push function

--- XML_Test/Adapter/XMLAdapter.cs
+++ XML_Test/Adapter/XMLAdapter.cs
@@ -32,25 +32,61 @@ namespace BH.Test.XML.Adapter
             Assert.IsTrue(adapter.FileName.Equals(file));
             Assert.IsTrue(adapter.FilePath.Equals(path));
         }
+
+        [TestMethod]
+        public void XML_Adapter_Push()
+        {
+  //Input for XMLAdapter
+            string file = "model";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            XMLAdapter adapter = new XMLAdapter(file, path);
+
+  //Creating BHoM object
+
+            //BHoMObject obj = new BHoMObject();
+            
+            IObject obj = new IObject();
+            System.Collections.Generic.IEnumerable<BH.oM.Base.IObject>;
+
+            //BH.oM.Geometry.Point obj = new oM.Geometry.Point();
+            //BH.oM.Geometry.Point obj = BH.Engine.XML.Convert.ToBHoM(pt);
+            //BH.oM.Base.IObject obj = BH.Engine.XML.Convert.ToBHoM(pt);
+
+  //Creating input for the Push function
+            string key = "newKey";
+            string tag = "newTag";
+            Dictionary<string, object> diction = new Dictionary<string, object>();
+            diction.Add(key, obj);
+
+   //Pushes data and Asserts properties
+            adapter.Push(obj, tag, diction);
+            Assert.IsTrue(adapter.GetType() == typeof(adapter.Push)); 
+        }
     }
 }
 
-//XMLAdapter adapter = new XMLAdapter();
-//Assert.IsTrue(XMLAdapter(name, path).Equals);
-//Assert.IsNotNull(XMLAdapter(name, path));
-//Assert.IsTrue(XMLAdapter.Equals("x", "y"));
-//Assert.IsTrue(FileName.Equals(xmlFileName));
-//Assert.IsTrue(x.XML_Adapter_FileName().Equals("Filename"));
-/*
-[TestClass]
-public class ToGBXMLTypeSurfaceType
-{
-    [TestMethod]
-    public void SurfaceType_Rooflight()
-    {
-        string x = "Rooflight";
-        Assert.IsTrue(x.ToGBXMLSurfaceType().Equals("OperableSkylight"));
-    }
-*/
 
-// Wrong namespace?
+
+
+
+/*
+// Choose object, push (object, show tag, Dictionary=items accessed by a key)
+public override List<IObject> Push(IEnumerable<IObject> objects, String tag = "", Dictionary<String, object> config = null)
+{
+    // Boolean that shows if the push function works
+    bool success = true;
+    // Show type of object, choose method after that
+    MethodInfo methodInfos = typeof(Enumerable).GetMethod("Cast");
+    // Get every type of objects
+    foreach (var typeGroup in objects.GroupBy(x => x.GetType()))
+    {
+        //get info of method
+        MethodInfo mInfo = methodInfos.MakeGenericMethod(new[] { typeGroup.Key });
+        
+        var list = mInfo.Invoke(typeGroup, new object[] { typeGroup });
+        success &= Create(list as dynamic, false);
+    }
+    // Set objects to list
+    return success ? objects.ToList() : new List<IObject>();
+}
+*/

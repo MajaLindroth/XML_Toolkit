85925ac73c20fe7c8907446945812b68a7752311
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-02-04 12:32:22 +0000

    Add test for converting points

--- XML_Test/Engine/Convert/Geometry_oM/Point.cs
+++ XML_Test/Engine/Convert/Geometry_oM/Point.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
  *
@@ -21,30 +21,43 @@
  */
 
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using BH.Adapter.XML;
+using BHG = BH.oM.Geometry;
+using BHX = BH.oM.XML;
+using BHE = BH.oM.Environment.Elements;
+
 using BH.Engine.XML;
-
-using BH.oM.Environment.Elements;
-
-using System.Collections.Generic;
-
-using BH.oM.XML;
 
 namespace BH.Test.XML
 {
     [TestClass]
-    public partial class ToBHoM
+    public class Point
     {
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_3coords()
+        public void ToGBXML_Point()
         {
-            //Test for setting 3 coordinates to a cartesian point, 
-            //to see if it returns a BHoM cartesian point with the right coordinates.
+            BHG.Point p = new BHG.Point();
+            p.X = 0;
+            p.Y = 10;
+            p.Z = 20;
+
+            BHX.CartesianPoint test = p.ToGBXML();
+
+            Assert.IsTrue(test.Coordinate.Length == 3);
+            Assert.IsTrue(test.Coordinate[0] == p.X.ToString());
+            Assert.IsTrue(test.Coordinate[1] == p.Y.ToString());
+            Assert.IsTrue(test.Coordinate[2] == p.Z.ToString());
+        }
+
+        [TestMethod]
+        public void ToBHoM_CartesianPoint_3coords()
+        {
+            //Test for setting 3 coordinates to a cartesian point, to see if it returns a BHoM cartesian point with the right coordinates.
 
             //Create cartesian point coordinates
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[3];
             coords[0] = "1";
             coords[1] = "2";
@@ -52,7 +65,7 @@ namespace BH.Test.XML
             pt.Coordinate = coords;
 
             //Converts to BHoM point
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             //See if the coordinates are set up correctly to the point
             Assert.IsTrue(bhomPt.X == 1);
@@ -61,16 +74,15 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_0coords()
+        public void ToBHoM_CartesianPoint_0coords()
         {
             //Test for 0 coordinates set on cartesian point
-
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[3];
 
             pt.Coordinate = coords;
 
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             Assert.IsTrue(bhomPt.X == 0);
             Assert.IsTrue(bhomPt.Y == 0);
@@ -78,17 +90,16 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_1coord()
+        public void ToBHoM_CartesianPoint_1coord()
         {
             //Test for 1 coordinates set on cartesian point
-
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[3];
             coords[0] = "1";
 
             pt.Coordinate = coords;
 
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             Assert.IsTrue(bhomPt.X == 1);
             Assert.IsTrue(bhomPt.Y == 0);
@@ -96,18 +107,17 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_2coords()
+        public void ToBHoM_CartesianPoint_2coords()
         {
             //Test for 2 coordinates set on cartesian point
-
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[3];
             coords[0] = "1";
             coords[1] = "2";
 
             pt.Coordinate = coords;
 
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             Assert.IsTrue(bhomPt.X == 1);
             Assert.IsTrue(bhomPt.Y == 2);
@@ -115,18 +125,17 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_2coords_2Array_numbers()
+        public void ToBHoM_CartesianPoint_2coords_2Array_numbers()
         {
             //Test for 3 coordinates set on cartesian point, but the array has only two numbers
-
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[2];
             coords[0] = "1";
             coords[1] = "2";
-            
+
             pt.Coordinate = coords;
 
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             Assert.IsTrue(bhomPt.X == 1);
             Assert.IsTrue(bhomPt.Y == 2);
@@ -134,16 +143,16 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_double()
+        public void ToBHoM_CartesianPoint_double()
         {
-            CartesianPoint pt = new CartesianPoint();
+            BHX.CartesianPoint pt = new BHX.CartesianPoint();
             string[] coords = new string[3];
             coords[0] = "1.12345";
             coords[1] = "2.38909";
             coords[2] = "3.84909";
             pt.Coordinate = coords;
 
-            BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+            BHG.Point bhomPt = pt.ToBHoM();
 
             Assert.IsTrue(bhomPt.X == 1.12345);
             Assert.IsTrue(bhomPt.Y == 2.38909);
@@ -151,19 +160,19 @@ namespace BH.Test.XML
         }
 
         [TestMethod]
-        public void TestToBHoM_CartesianPoint_stress()
+        public void ToBHoM_CartesianPoint_stress()
         {
             Random rand = new Random();
             for (int i = 0; i < 1000; i++)
             {
-                CartesianPoint pt = new CartesianPoint();
+                BHX.CartesianPoint pt = new BHX.CartesianPoint();
                 string[] coords = new string[3];
                 coords[0] = (rand.NextDouble() * 100).ToString();
                 coords[1] = (rand.NextDouble() * 100).ToString();
                 coords[2] = (rand.NextDouble() * 100).ToString();
                 pt.Coordinate = coords;
 
-                BH.oM.Geometry.Point bhomPt = BH.Engine.XML.Convert.ToBHoM(pt);
+                BHG.Point bhomPt = pt.ToBHoM();
                 Assert.IsTrue(bhomPt.X == (System.Convert.ToDouble(coords[0])));
                 Assert.IsTrue(bhomPt.Y == (System.Convert.ToDouble(coords[1])));
                 Assert.IsTrue(bhomPt.Z == (System.Convert.ToDouble(coords[2])));
